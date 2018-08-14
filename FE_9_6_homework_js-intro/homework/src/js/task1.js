b4c323e4e1946f66894e2f6e2d857e19c246acf0
Olha Holysh <olgaolha89@ukr.net>
2018-08-14 20:45:35 +0300

    fix eslint errors

--- FE_9_6_homework_js-intro/homework/src/js/task1.js
+++ FE_9_6_homework_js-intro/homework/src/js/task1.js
@@ -1,7 +1,7 @@
-var money = +prompt("Please, enter amount of money");
-var dis = +prompt("Please, enter discount");
-var saved = parseFloat((money * (dis / 100)).toFixed(2));
-var withDiscount = parseFloat((money - saved).toFixed(2));
-console.log(money <= 0 ? "Invalid data" : "Price without discount: " + money + "\nDiscount: " +
-    +dis + "%" + "\nPrice with discount: " + withDiscount + "\nSaved: " + saved);
+const money = +prompt('Please, enter amount of money');
+const dis = +prompt('Please, enter discount');
+const saved = parseFloat((money * (dis / 100)).toFixed(2));
+const withDiscount = parseFloat((money - saved).toFixed(2));
+console.log(money <= 0 ? 'Invalid data' : 'Price without discount: ' + money + '\nDiscount: ' +
+    +dis + '%' + '\nPrice with discount: ' + withDiscount + '\nSaved: ' + saved);
 
--- FE_9_6_homework_js-intro/homework/src/js/task2.js
+++ FE_9_6_homework_js-intro/homework/src/js/task2.js
@@ -1,7 +1,11 @@
-var a = +prompt("Please, enter length a");
-var b = +prompt("Please, enter length b");
-var Y = +prompt("Please, enter angle");
-var c = parseFloat((Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2) - (2 * b * a) * Math.cos(Y * Math.PI / 180))).toFixed(2));
-var P = a + b + c;
-var S = parseFloat((0.5 * (a * b * Math.sin(Y * Math.PI / 180))).toFixed(2));
-console.log((a <= 0 || b <= 0 || Y <= 0) ? "Invalid data" : "c length: " + c + "\nTriangle square: " + S + "\nTriangle perimeter: " + P);
+const a = +prompt('Please, enter length a');
+const b = +prompt('Please, enter length b');
+const Y = +prompt('Please, enter angle');
+const angleSum = 180;
+const squareHelper = 0.5;
+const c = parseFloat(Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2) - 2 * b * a * Math.cos(Y * Math.PI / angleSum))
+    .toFixed(2));
+const P = a + b + c;
+const S = parseFloat((squareHelper * (a * b * Math.sin(Y * Math.PI / angleSum))).toFixed(2));
+console.log(a <= 0 || b <= 0 || Y <= 0 ? 'Invalid data' : 'c length: ' + c + '\nTriangle square: ' +
+    S + '\nTriangle perimeter: ' + P);
