9150d5853f3a569c10131978a3c22313a88ff17d
Olha Holysh <olgaolha89@ukr.net>
2018-08-15 17:26:33 +0300

    update validation in console log

--- FE_9_6_homework_js-intro/homework/src/js/task1.js
+++ FE_9_6_homework_js-intro/homework/src/js/task1.js
@@ -2,6 +2,5 @@ const money = +prompt('Please, enter amount of money');
 const discount = +prompt('Please, enter discount');
 const saved = parseFloat((money * (discount / 100)).toFixed(2));
 const withDiscount = parseFloat((money - saved).toFixed(2));
-console.log(money <= 0 ? 'Invalid data' : 'Price without discount: ' + money + '\nDiscount: ' +
-    +discount + '%' + '\nPrice with discount: ' + withDiscount + '\nSaved: ' + saved);
-
+console.log(money > 0 ? 'Price without discount: ' + money + '\nDiscount: ' +
+    +discount + '%' + '\nPrice with discount: ' + withDiscount + '\nSaved: ' + saved :'Invalid data');
