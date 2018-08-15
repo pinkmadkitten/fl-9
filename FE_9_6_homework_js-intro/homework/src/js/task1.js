91f5af75592ba1c451a82aeb787a3a1a782cc514
Olha Holysh <olgaolha89@ukr.net>
2018-08-15 13:57:41 +0300

    change variable name

--- FE_9_6_homework_js-intro/homework/src/js/task1.js
+++ FE_9_6_homework_js-intro/homework/src/js/task1.js
@@ -1,6 +1,6 @@
 const money = +prompt('Please, enter amount of money');
 const discount = +prompt('Please, enter discount');
-const saved = parseFloat((money * (dis / 100)).toFixed(2));
+const saved = parseFloat((money * (discount / 100)).toFixed(2));
 const withDiscount = parseFloat((money - saved).toFixed(2));
 console.log(money <= 0 ? 'Invalid data' : 'Price without discount: ' + money + '\nDiscount: ' +
     +discount + '%' + '\nPrice with discount: ' + withDiscount + '\nSaved: ' + saved);
