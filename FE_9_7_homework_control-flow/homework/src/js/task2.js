7d4991e62a740516e4b5d6c2a54040e841e35dc7
Olha Holysh <olgaolha89@ukr.net>
2018-08-17 20:48:23 +0300

    add task 2

--- FE_9_7_homework_control-flow/homework/src/js/task2.js
+++ FE_9_7_homework_control-flow/homework/src/js/task2.js
@@ -1,1 +1,54 @@
-// Your code goes here
+const availableAttempts = 3;
+const rangeStart = 0;
+let attempt = 0;
+let maxPrize = 10;
+let possiblePrize = maxPrize;
+let prize = 0;
+let question = confirm(`Do you want to play a game?`);
+while (question) {
+    let rangeEnd = 5;
+    let randomNum = random(rangeStart, rangeEnd);
+    while (question && attempt < availableAttempts) {
+        console.log(randomNum);
+        let userNumber = +prompt(
+`Please enter the number from ${rangeStart} to ${rangeEnd}
+Attempts left: ${availableAttempts - attempt}
+Total prize: ${prize}$
+Possible prize on current attempt: ${possiblePrize}$`);
+        if (userNumber === randomNum) {
+            prize += possiblePrize;
+            maxPrize = maxPrize * 3;
+            possiblePrize = maxPrize;
+            question = confirm(`Congrats! You're prize is ${prize}$. Do you want to to continue?`);
+            attempt = 0;
+            rangeEnd = rangeEnd * 2;
+            randomNum = random(rangeStart, rangeEnd);
+        } else if (!userNumber) {
+            alert(`Thank you for a game. Your prize is: ${prize}`);
+            question = confirm(`Do you want to play again?`);
+            attempt = 0;
+        } else {
+            possiblePrize = Math.floor(possiblePrize / 2);
+            attempt++;
+        }
+    }
+    if (attempt >= availableAttempts) {
+        alert(`Thank you for a game. Your prize is: ${prize}`);
+        question = confirm(`Do you want to play again?`);
+        attempt = 0;
+    }
+}
+
+if (!question) {
+    alert('You did not become a millionaire, but can.');
+}
+
+function random(min, max) {
+    let fixture = 1; // We add 1 in order to include max number in the range
+    return Math.floor(Math.random() * (max - min + fixture) - min);
+}
+
+
+
+
+
